a03a3f1f6583c2077cb4defd29ad22cb4db0eff9
MaVirgil <magnusvirgil@hotmail.com>
2026-04-20 09:36:26 +0200

    initial commit

---
+++ src/main/java/com/mavi/themaurus/ThemaurusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mavi.themaurus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemaurusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThemaurusBackendApplication.class, args);
+    }
+
+}
